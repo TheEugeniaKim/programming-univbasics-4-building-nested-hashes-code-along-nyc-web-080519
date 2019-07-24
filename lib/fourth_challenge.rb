d166998a5e7b5cc9ed5c51258999a0e8e6bdeb06
Eugenia Kim <elk303@nyu.edu>
2019-07-24 22:52:38 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,7 +5,7 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-        {name: "Benvolio", age: "17", attitude: "worried"}
+        {name: "Benvolio", age: "17", attitude: "worried"},
         {name: "Mercutio", age: "18", attitude: "hot-headed"}
         ]
    }, 
@@ -16,7 +16,6 @@ def fourth_challenge
       :heroine_friends => [
         {name: "Steven", age: "30", attitude: "confused"},
         {name: "Nurse", age: "44", attitude: "worried"}
-        ]
         }
       ]
    }
